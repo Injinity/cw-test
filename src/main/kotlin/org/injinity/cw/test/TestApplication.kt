46d920a55a0dd0d2f0980a1a9410e50ce484460d
Dastroo <dreslerdawid01@gmail.com>
2022-11-20 18:22:05 +0100

    Initial commit

---
+++ src/main/kotlin/org/injinity/cw/test/TestApplication.kt
@@ -0,0 +1,26 @@
+package org.injinity.cw.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@SpringBootApplication
+@EnableEurekaClient
+class TestApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestApplication>(*args)
+}
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+
+    @GetMapping
+    fun test(): String = "Resource Test String"
+}
