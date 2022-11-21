b27a25d314a5d7cf705a6b68a3f6ba063a5de663
Dastroo <dreslerdawid01@gmail.com>
2022-11-21 11:20:04 +0100

    trying out jib

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    id("com.google.cloud.tools.jib") version "3.1.4"
 }
 
 group = "org.injinity.cw"
@@ -50,6 +51,26 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+jib {
+    from {
+        image = "amazoncorretto:17"
+        platforms {
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
+    }
+    to {
+        image = "remote.injinity.org/injinity/${project.name}"
+        auth {
+            username = project.findProperty("registryUsername") as String?
+            password = project.findProperty("registryPassword") as String?
+        }
+    }
+}
+
+/*
 tasks.withType<BootBuildImage> {
     builder = "dashaun/java-native-builder-arm64:latest"
     imageName = "remote.injinity.org/injinity/${project.name}"
@@ -60,4 +81,4 @@ tasks.withType<BootBuildImage> {
             password = project.findProperty("registryPassword") as String?
         }
     }
-}
+}*/
