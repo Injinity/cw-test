b3e268c7a012907aed6e34461413e4a8af25ac9e
Dastroo <dreslerdawid01@gmail.com>
2022-12-23 11:47:25 +0100

    added trace level logging

--- src/main/kotlin/org/injinity/cw/test/TestApplication.kt
+++ src/main/kotlin/org/injinity/cw/test/TestApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity.http
 import org.springframework.security.config.web.server.invoke
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.web.bind.annotation.GetMapping
